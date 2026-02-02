4d5de3de772520d8444165bd816d4bcbc27a4814
Emeson Santos <emesonneves111@gmail.com>
2026-02-01 22:47:12 -0300

    fix: pequeno ajuste no nome da documentação do swagger

--- backend-api/app/main.py
+++ backend-api/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.api import api_router
 
-app = FastAPI(title="Intuitivr Care API", version="1.0.0")
+app = FastAPI(title="Intuitive Care API", version="1.0.0")
 
 origins = ["*"]
 
